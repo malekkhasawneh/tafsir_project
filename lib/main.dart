a6dc77a63905cf7a4843ffb8ea00fa945f36bb2b
mkhasawneh <malekmamoon341@gmail.com>
2021-10-18 10:00:07 +0300

    GreateTafsir

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:tafsir_project/SplachScreen.dart';
 import 'package:tafsir_project/features/Show_Quran_Pages/Presentation/Pages/HomePage.dart';
 
 void main() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       routes: {
         HomePage.routeName: (context) => HomePage(),
